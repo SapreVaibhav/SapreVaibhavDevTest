fbef5e1dd176670708f4be3c9cdfe7b1bfabff4b
SapreVaibhav <46812946+SapreVaibhav@users.noreply.github.com>
2019-01-29 22:39:23 +0530

    Add files via upload

--- SapreVaibhavDevTest/FileDataController/FileDataProcessor.cs
+++ SapreVaibhavDevTest/FileDataController/FileDataProcessor.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ThirdPartyTools;
 
 namespace FileDataController
@@ -15,36 +12,39 @@ namespace FileDataController
     {
         string strReturnData = "";
 
-        public string ProcessFileData(string strInParam, string[] arVersion, string[] arSize)
+        public string ProcessFileData(string strInParam)
         {
             try
             {
                 string[] strParam = strInParam.Split(' ');
                 FileDetails oFileDetails = new FileDetails();
+                string strMsgValidInputs = "Please, enter valid Inputs.";
 
-                //string[] arrVersion = ConfigurationSettings.AppSettings["VersionCollection"].Split(',');
-                //string[] arrSize = ConfigurationSettings.AppSettings["SizeCollection"].Split(',');
+                string[] arrVersion = ConfigurationSettings.AppSettings["VersionCollection"].Split(',');
+                string[] arrSize = ConfigurationSettings.AppSettings["SizeCollection"].Split(',');
 
-                //string[] arrVersion = Convert.ToString("-v,--v,/v,--version").Split(',');
-                //string[] arrSize = Convert.ToString("-s,--s,/s,--size").Split(',');
-
-                if (strInParam.Length < 2)
+               
+                if (strInParam.Length <= 2)
                 {
-                    strReturnData = "Please, enter valid Inputs.";
+                    strReturnData = strMsgValidInputs;
                 }
-                else if (arSize.Contains(strParam[0].ToLower()))
+                else if (arrSize.Contains(strParam[0].ToLower()))
                 {
                     strReturnData = String.Concat("File Size: ", Convert.ToString(oFileDetails.Size(strParam[1].Trim())));
                 }
-                else if (arVersion.Contains(strParam[0].ToLower()))
+                else if (arrVersion.Contains(strParam[0].ToLower()))
                 {
                     strReturnData = String.Concat("File Version: ", oFileDetails.Version(strParam[1].Trim()));
+                }
+                else
+                {
+                    strReturnData = strMsgValidInputs;
                 }
             }
             catch(Exception Ex)
             {
-                //strReturnData = ConfigurationSettings.AppSettings["CustomErrorMsg"];
-                strReturnData = "Some error has occured. Please, try again.";
+                strReturnData = ConfigurationSettings.AppSettings["CustomErrorMsg"];
+                
             }
 
             return strReturnData;
--- SapreVaibhavDevTest/FileDataController/IFileDataProcessor.cs
+++ SapreVaibhavDevTest/FileDataController/IFileDataProcessor.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FileDataController
+﻿namespace FileDataController
 {
-    
+
     public interface IFileDataProcessor
     {
-        string ProcessFileData(string strInParam, string[] arVersion, string[] arSize);
+        string ProcessFileData(string strInParam);
     }
 
 }
