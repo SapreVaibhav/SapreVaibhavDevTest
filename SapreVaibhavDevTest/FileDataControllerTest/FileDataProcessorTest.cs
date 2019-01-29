fd9f1ee2903216d9f5d037a373564ce46b35a9be
SapreVaibhav <46812946+SapreVaibhav@users.noreply.github.com>
2019-01-29 22:41:10 +0530

    Add files via upload

--- SapreVaibhavDevTest/FileDataControllerTest/FileDataProcessorTest.cs
+++ SapreVaibhavDevTest/FileDataControllerTest/FileDataProcessorTest.cs
@@ -1,7 +1,5 @@
-﻿using System;
+﻿using FileDataController;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FileDataController;
-using System.Configuration;
 
 namespace FileDataControllerTest
 {
@@ -10,35 +8,60 @@ namespace FileDataControllerTest
     {
         FileDataProcessor oFileDataProcessor = new FileDataProcessor();
         string strOutPut = null;
-        string[] arrVersion = Convert.ToString("-v,--v,/v,--version").Split(',');
-        string[] arrSize = Convert.ToString("-s,--s,/s,--size").Split(',');
-
-
+       
         [TestMethod]
         public void ProcessFileDataValidVersion()
         {
-            strOutPut = oFileDataProcessor.ProcessFileData("-v c:/demo.txt",arrVersion,arrSize);
+            strOutPut = oFileDataProcessor.ProcessFileData("-v c:/demo.txt");
             Assert.IsTrue(strOutPut.Contains("File Version"));
         }
 
         [TestMethod]
         public void ProcessFileDataValidSize()
         {
-            strOutPut = oFileDataProcessor.ProcessFileData("-s c:/demo.txt", arrVersion, arrSize);
+            strOutPut = oFileDataProcessor.ProcessFileData("-s c:/demo.txt");
             Assert.IsTrue(strOutPut.Contains("File Size"));
         }
 
         [TestMethod]
         public void ProcessFileDataInValidVersion()
         {
-            strOutPut = oFileDataProcessor.ProcessFileData("version c:/demo.txt", arrVersion, arrSize);
+            strOutPut = oFileDataProcessor.ProcessFileData("v c:/demo.txt");
             Assert.IsFalse(strOutPut.Contains("File Version")); 
         }
 
         [TestMethod]
         public void ProcessFileDataInValidSize()
         {
-            strOutPut = oFileDataProcessor.ProcessFileData("version c:/demo.txt", arrVersion, arrSize);
+            strOutPut = oFileDataProcessor.ProcessFileData("s c:/demo.txt");
+            Assert.IsFalse(strOutPut.Contains("File Size"));
+        }
+
+        [TestMethod]
+        public void ProcessFileDataCorrectVersion()
+        {
+            strOutPut = oFileDataProcessor.ProcessFileData("--v c:/demo.txt");
+            Assert.IsTrue(strOutPut.Contains("File Version"));
+        }
+
+        [TestMethod]
+        public void ProcessFileDataCorrectSize()
+        {
+            strOutPut = oFileDataProcessor.ProcessFileData("--s c:/demo.txt");
+            Assert.IsTrue(strOutPut.Contains("File Size"));
+        }
+
+        [TestMethod]
+        public void ProcessFileDataWrongVersion()
+        {
+            strOutPut = oFileDataProcessor.ProcessFileData("version ");
+            Assert.IsFalse(strOutPut.Contains("File Version"));
+        }
+
+        [TestMethod]
+        public void ProcessFileDataWrongSize()
+        {
+            strOutPut = oFileDataProcessor.ProcessFileData("size ");
             Assert.IsFalse(strOutPut.Contains("File Size"));
         }
     }
